8f2467c86c2c8b37a9f59ed174a975ac1ce808ea
Tom Maurer <tmaurer@nait.ca>
2025-09-10 11:29:43 -0600

    created skeleton for exercise

--- FunctionsAndEvents/function-intro-example/js/main.js
+++ FunctionsAndEvents/function-intro-example/js/main.js
@@ -1,2 +1,32 @@
 // this is our javascript file.
 console.log("Script loaded");
+
+// TODO: Prompt the User for their username
+const getUsername = function () {
+  // TODO: Prompt the user
+  // TODO: return the user's input
+};
+
+// TODO: Prompt the User for a description
+function getDescription() {
+  // TODO: Prompt the user
+  // TODO: Return the user's input
+}
+
+// TODO: Display the Username
+const updateUsername = () => {
+  // TODO: Call our prompt function
+  // TODO: Update element with the new text
+};
+
+// TODO: Display description
+const updateDescription = () => {
+  // TODO: Call our prompt function
+  // TODO: Update element with the new text
+};
+
+// TODO: Create a function that updates an element to display new text
+const updateElementText = (selector, text) => {
+  // TODO: Select the element with the given selector
+  // TODO: Update the innerText of the selected element
+};
