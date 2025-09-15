2589caa43dc9b2c28a2a0f99c463a495e29a8184
Tom Maurer <tmaurer@nait.ca>
2025-09-15 14:11:34 -0600

    completed the event default prevention

--- FunctionsAndEvents/events-basics-START/js/main.js
+++ FunctionsAndEvents/events-basics-START/js/main.js
@@ -3,17 +3,26 @@ console.log("Javascript linked");
 // Select the add count button
 let addButton = document.querySelector(".btn-increment");
 
-let count = 0;
 // Write an event handler for incrementing count
 function addToCount() {
   console.log("This event was handled");
-  // Increment the count variable
-  count += 1; // Same as count = count + 1;
   // Select the count text
   let counterText = document.querySelector(".counter");
   // Update the count text with the new value
-  counterText.innerText = count;
+  counterText.innerText = parseInt(counterText.innerText) + 1;
 }
 
 // Bind the event listener to the button
 addButton.addEventListener("click", addToCount);
+
+// Prevent default behaviour on link
+document.querySelector(".link").addEventListener("click", (event) => {
+  event.preventDefault();
+
+  // Now I can replace the default with whatever I like
+});
+
+// Prevent default behaviour on form
+document.querySelector(".form").addEventListener("submit", (e) => {
+  e.preventDefault();
+});
