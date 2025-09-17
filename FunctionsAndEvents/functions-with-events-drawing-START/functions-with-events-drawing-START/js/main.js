884c756678ff45e8f5b5dfa61f48dc23fbcc50c1
Tom Maurer <tmaurer@nait.ca>
2025-09-17 10:37:44 -0600

    finished drawing example

--- FunctionsAndEvents/functions-with-events-drawing-START/functions-with-events-drawing-START/js/main.js
+++ FunctionsAndEvents/functions-with-events-drawing-START/functions-with-events-drawing-START/js/main.js
@@ -7,14 +7,18 @@ let main = document.querySelector("main");
 
 // add an event listener to the body on the mousemove
 body.addEventListener("mousemove", (event) => {
-  console.log(event.x, event.y);
   // select the green-box
+  let greenBox = document.querySelector(".green-box");
   // modify the style attributes of the green-box of the
+  greenBox.style.position = "absolute";
   // top and left with the position of the event.
+  greenBox.style.top = `${event.y}px`;
+  greenBox.style.left = `${event.x}px`;
 });
 
 body.addEventListener("click", (event) => {
-  let newBoxTemplate = `<div class="blue-box" style="top: ${Y}px; left: ${X}px"></div>`;
+  let newBoxTemplate = `<div class="blue-box" style="top: ${event.y}px; left: ${event.x}px"></div>`;
   // add an event listener to click that will add a blue box to the main innerHTML
   // at the position of the mouse.
+  main.innerHTML += newBoxTemplate;
 });
