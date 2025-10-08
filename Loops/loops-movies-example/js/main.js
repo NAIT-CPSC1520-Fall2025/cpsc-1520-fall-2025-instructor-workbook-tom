0666d027f74218f8243c2fef4be9b2976c725e1c
Tom Maurer <tmaurer@nait.ca>
2025-10-08 10:31:35 -0600

    missing some things committing now

---
+++ Loops/loops-movies-example/js/main.js
@@ -0,0 +1,45 @@
+// this is our javascript file.
+
+let movieFilterForm = document.querySelector("#movie-filter-form");
+
+// focus on the filter element
+movieFilterForm.elements["filter-query"].focus();
+
+// intercept the form.
+movieFilterForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  let filter = event.target.elements["filter-query"].value;
+
+  filterItems(filter);
+});
+
+// select all movie list items the descendant css selector
+// .top-movies-list li
+
+// get the form value and call the function filterItems
+
+/*
+
+
+We're going to create a function named "filterItems"
+that check each item contains our search query
+- if it's not then we're going to add the "hidden-item" class
+- if does include it we're going to remove the "hidden-item" class.
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/includes
+
+*/
+
+const filterItems = (filterValue) => {
+  console.log("this was filtered with: " + filterValue);
+
+  let allMovieElements = document.querySelectorAll(".top-movies-list li");
+  for (let i = 0; i < allMovieElements.length; i++) {
+    // Get the indexed movie element
+    let movieElement = allMovieElements[i];
+    // check if the innerText of that element .includes() the filter
+    // if it does remove .hidden-item
+    // else add .hidden-item
+  }
+};
